35b7a9ec0ab91782d58eb4c1d889c780ef712324
DannyArends <Danny.Arends@gmail.com>
2015-09-08 09:55:33 +0200

    Update the settings file to contain the path to PLINK and pyLMM

--- my_settings.py
+++ my_settings.py
@@ -1,4 +1,5 @@
 DB_URI="mysql://GN:mypass@localhost/db_webqtl"
 SECRET_HMAC_CODE="secretkey"
-PYLMM_PATH="/home/zas1024/gene/wqflask/wqflask/my_pylmm/pyLMM"
+PYLMM_PATH="/home/zas1024/pyLMM"
+PLINK_PATH="/home/zas1024/plink-1.07-x86_64"
 SERVER_PORT=5003
