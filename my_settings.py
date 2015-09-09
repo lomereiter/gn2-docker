f474abe060860774def113b38ebc221c45916501
DannyArends <Danny.Arends@gmail.com>
2015-09-09 16:38:08 +0200

    Adding more settings to the my_settings.py to make sure we have set a secret key for Flask

--- my_settings.py
+++ my_settings.py
@@ -3,3 +3,7 @@ SECRET_HMAC_CODE="secretkey"
 PYLMM_PATH="/home/zas1024/pyLMM"
 PLINK_PATH="/home/zas1024/plink-1.07-x86_64"
 SERVER_PORT=5003
+
+#SECURITY_PASSWORD_HASH="bcrypt"
+#SESSION_TYPE="filesystem"
+#SECRET_KEY="secretkey"
