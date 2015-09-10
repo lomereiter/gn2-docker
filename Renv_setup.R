f09b72b0c563b8014c9a5a488e4af99c5a64e07f
DannyArends <Danny.Arends@gmail.com>
2015-09-10 21:09:03 +0200

    Adding GO.db as missing dependancy, and we now exit the script cleanly

--- Renv_setup.R
+++ Renv_setup.R
@@ -1,8 +1,7 @@
 # Install packages required for a functioning genenetwork2 R instance
 # Author /  Maintainer: Danny Arends <Danny.Arends@gmail.com>
 
-install.packages("devtools")      # Install R packages directly from Github
-install.packages("qtl")           # R/QTL required for single and multiple QTL mapping
+install.packages("qtl", repos = "http://cran.us.r-project.org")           # R/QTL required for single and multiple QTL mapping
 
 # Bioconductor is required for many different packages
 source("http://bioconductor.org/biocLite.R")
@@ -12,7 +11,11 @@ biocLite(ask = FALSE)
 
 # Install required WGCNA packages from bioconductor
 biocLite("impute", suppressUpdates = TRUE, ask = FALSE)
+biocLite("GO.db", suppressUpdates = TRUE, ask = FALSE)
 biocLite("preprocessCore", suppressUpdates = TRUE, ask = FALSE) # Seems to be an unlisted missing dependency
 
 # WGCNA is on CRAN
-install.packages("WGCNA")
+install.packages("WGCNA", repos = "http://cran.us.r-project.org")           # R/QTL required for single and multiple QTL mapping
+
+# All done quit without error
+q("no")
